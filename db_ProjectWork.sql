d5e2c3dfd5d8ddc6648988bedb24973a9dc27dc4
nurkumalabudif <nurkumala_fajrin_24rpl@student.smktelkom-mlg.sch.id>
2018-01-05 10:40:20 +0700

    Fixing Bug Admin DB

--- db_ProjectWork.sql
+++ db_ProjectWork.sql
@@ -1,61 +1,98 @@
-/*==============================================================*/
-/* DBMS name:      MySQL 5.0                                    */
-/* Created on:     05/01/2018 10.05.42                          */
-/*==============================================================*/
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Inang: 127.0.0.1
+-- Waktu pembuatan: 05 Jan 2018 pada 04.38
+-- Versi Server: 5.5.27
+-- Versi PHP: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
-drop table if exists ADMIN;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-drop table if exists CUSTOMER;
+--
+-- Basis data: `db_projectwork`
+--
 
-drop table if exists KONFIRMASI_PEMBAYARAN;
+-- --------------------------------------------------------
 
-/*==============================================================*/
-/* Table: ADMIN                                                 */
-/*==============================================================*/
-create table ADMIN
-(
-   USERNAME             varchar(500) not null,
-   PASSWORD             varchar(500),
-   primary key (USERNAME)
-);
+--
+-- Struktur dari tabel `admin`
+--
 
-/*==============================================================*/
-/* Table: CUSTOMER                                              */
-/*==============================================================*/
-create table CUSTOMER
-(
-   NAMA_CUST            varchar(500),
-   TELP                 numeric(8,0),
-   TANGGAL              date not null,
-   USERNAME             varchar(500),
-   JENIS_ACARA          varchar(500),
-   JAM                  varchar(500),
-   KETERANGAN           text,
-   primary key (TANGGAL)
-);
+CREATE TABLE IF NOT EXISTS `admin` (
+  `USERNAME` varchar(500) NOT NULL,
+  `PASSWORD` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`USERNAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*==============================================================*/
-/* Table: KONFIRMASI_PEMBAYARAN                                 */
-/*==============================================================*/
-create table KONFIRMASI_PEMBAYARAN
-(
-   KD_BOOKING           varchar(500) not null,
-   TANGGAL              date,
-   USERNAME             varchar(500),
-   NAMA_KONFIR          varchar(500),
-   NO_REKENING          numeric(8,0),
-   JML_UANG             numeric(8,0),
-   STATUS               text,
-   primary key (KD_BOOKING)
-);
+--
+-- Dumping data untuk tabel `admin`
+--
 
-alter table CUSTOMER add constraint FK_MEMESAN foreign key (USERNAME)
-      references ADMIN (USERNAME) on delete restrict on update restrict;
+INSERT INTO `admin` (`USERNAME`, `PASSWORD`) VALUES
+('admin', '21232f297a57a5a743894a0e4a801fc3');
 
-alter table KONFIRMASI_PEMBAYARAN add constraint FK_MENGECEK foreign key (USERNAME)
-      references ADMIN (USERNAME) on delete restrict on update restrict;
+-- --------------------------------------------------------
 
-alter table KONFIRMASI_PEMBAYARAN add constraint FK_MENGONFIRMASI foreign key (TANGGAL)
-      references CUSTOMER (TANGGAL) on delete restrict on update restrict;
+--
+-- Struktur dari tabel `customer`
+--
 
+CREATE TABLE IF NOT EXISTS `customer` (
+  `NAMA_CUST` varchar(500) DEFAULT NULL,
+  `TELP` decimal(8,0) DEFAULT NULL,
+  `TANGGAL` date NOT NULL,
+  `USERNAME` varchar(500) DEFAULT NULL,
+  `JENIS_ACARA` varchar(500) DEFAULT NULL,
+  `JAM` varchar(500) DEFAULT NULL,
+  `KETERANGAN` text,
+  PRIMARY KEY (`TANGGAL`),
+  KEY `FK_MEMESAN` (`USERNAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konfirmasi_pembayaran`
+--
+
+CREATE TABLE IF NOT EXISTS `konfirmasi_pembayaran` (
+  `KD_BOOKING` varchar(500) NOT NULL,
+  `TANGGAL` date DEFAULT NULL,
+  `USERNAME` varchar(500) DEFAULT NULL,
+  `NAMA_KONFIR` varchar(500) DEFAULT NULL,
+  `NO_REKENING` decimal(8,0) DEFAULT NULL,
+  `JML_UANG` decimal(8,0) DEFAULT NULL,
+  `STATUS` text,
+  PRIMARY KEY (`KD_BOOKING`),
+  KEY `FK_MENGECEK` (`USERNAME`),
+  KEY `FK_MENGONFIRMASI` (`TANGGAL`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `FK_MEMESAN` FOREIGN KEY (`USERNAME`) REFERENCES `admin` (`USERNAME`);
+
+--
+-- Ketidakleluasaan untuk tabel `konfirmasi_pembayaran`
+--
+ALTER TABLE `konfirmasi_pembayaran`
+  ADD CONSTRAINT `FK_MENGONFIRMASI` FOREIGN KEY (`TANGGAL`) REFERENCES `customer` (`TANGGAL`),
+  ADD CONSTRAINT `FK_MENGECEK` FOREIGN KEY (`USERNAME`) REFERENCES `admin` (`USERNAME`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
